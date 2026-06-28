1cf9037628dc6db3542a7afa818491c04e093319
iqubik <iqubik@gmail.com>
2026-06-28 21:33:46 +0300

    feat(hydraroute): streamline geo refresh and Neo installation lifecycle
    Приводит в порядок geo-refresh и жизненный цикл HydraRoute Neo: расписание обновлений, post-start, установка из feed и API/install-update контракт.

--- frontend/src/lib/components/hrneo/index.ts
+++ frontend/src/lib/components/hrneo/index.ts
@@ -7,4 +7,3 @@ export { default as HrNeoGeoTagPicker } from './HrNeoGeoTagPicker.svelte';
 export { default as HrNeoGeoDataView } from './HrNeoGeoDataView.svelte';
 export { default as HrNeoSettingsView } from './HrNeoSettingsView.svelte';
 export { default as HrNeoDisabledTagsView } from './HrNeoDisabledTagsView.svelte';
-export { default as HrNeoGeoRefreshSettings } from './HrNeoGeoRefreshSettings.svelte';
